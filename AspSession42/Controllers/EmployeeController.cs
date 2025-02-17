fbc9c870c6b90bbea2df6f656d765d9165374633
Svetlana184 <sveta.kgd.39@gmail.com>
2025-02-17 16:24:22 +0200

    Добавьте файлы проекта.

---
+++ AspSession42/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using AspSession42.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspSession42.Controllers
+{
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    {
+        private RoadOfRussiaContext db;
+        public EmployeeController(RoadOfRussiaContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        [Route("Employees")]
+        public IQueryable GetEmployees()
+        {
+            var emp = from e in db.Employees
+                      select new
+                      {
+                          FIO = e.Surname + " " + e.FirstName + " " + e.SecondName,
+                          Email = e.Email,
+                          Phone = e.PhoneWork,
+                          Position = e.Position,
+                          BirthDay = DateTime.Parse(e.BirthDay.ToString()!).ToString("M"),
+                      };
+            return emp.AsQueryable();
+        }
+    }
+}
---
+++ AspSession42/Controllers/EventController.cs
@@ -0,0 +1,35 @@
+﻿using AspSession42.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspSession42.Controllers
+{
+    [ApiController]
+    [Authorize]
+    public class EventController : Controller
+    {
+        private RoadOfRussiaContext db;
+        public EventController(RoadOfRussiaContext db)
+        {
+            this.db = db;
+        }
+        [HttpGet]
+        [Route("Events")]
+        public IQueryable GetEvents()
+        {
+            var events = from e in db.Events
+                         select new
+                         {
+                             EventName = e.EventName,
+                             EventDate = e.DateOfEvent.ToString("dd.MM.yyyy"),
+                             Author = db.Employees.FirstOrDefault(p => p.IdEmployee.ToString() == e.EventManagers)!.Surname +
+                             " " + db.Employees.FirstOrDefault(p => p.IdEmployee.ToString() == e.EventManagers)!.FirstName.Substring(0, 1) + ". "
+                             + db.Employees.FirstOrDefault(p => p.IdEmployee.ToString() == e.EventManagers)!.SecondName!.Substring(0, 1) + ".",
+                             Description = e.EventDescription
+                         };
+            return events.AsQueryable();
+
+        }
+
+    }
+}
