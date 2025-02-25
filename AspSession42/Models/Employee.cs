15b4c23b95c466e09f4445118ed04c7daf363833
Svetlana184 <sveta.kgd.39@mail.com>
2025-02-25 12:27:16 +0200

    api is working!!!!!!!!

--- AspSession42/Models/Employee.cs
+++ AspSession42/Models/Employee.cs
@@ -34,7 +34,7 @@ public partial class Employee
     public int? IdBoss { get; set; }
 
     public string? Password { get; set; }
-    public bool? IsFired { get; set; }
+    public DateTime? IsFired { get; set; }
 
     public virtual ICollection<Calendar> CalendarIdAlternateNavigations { get; set; } = new List<Calendar>();
 
